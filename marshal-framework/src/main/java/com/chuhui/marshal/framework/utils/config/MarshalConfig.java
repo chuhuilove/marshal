971fc459911a05dcbcb86aede034edf03c4dd76f
chuhui <lovehui131125@gmail.com>
2019-11-18 20:06:22 +0800

    add log

--- marshal-framework/src/main/java/com/chuhui/marshal/framework/utils/config/MarshalConfig.java
+++ marshal-framework/src/main/java/com/chuhui/marshal/framework/utils/config/MarshalConfig.java
@@ -2,9 +2,12 @@ package com.chuhui.marshal.framework.utils.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
+import java.util.LinkedHashMap;
 
 import static com.chuhui.marshal.framework.utils.FileUtils.openFileAsInputStream;
 
@@ -19,22 +22,40 @@ import static com.chuhui.marshal.framework.utils.FileUtils.openFileAsInputStream
 @Getter
 @Setter
 public class MarshalConfig {
+    final static  private Logger logger = LoggerFactory.getLogger(MarshalConfig.class);
+
+    private MarshalBasicConfig marshalBasic;
 
 
-    private Boolean standalone;
-    private Integer serverPort;
-
-
-
-    public MarshalConfig(String configFileName){
-
-        InputStream fileStream = openFileAsInputStream(configFileName);
-        Yaml yaml=new Yaml();
-        MarshalConfig config=  yaml.load(fileStream);
+    public MarshalConfig() {
     }
 
 
+    public static MarshalConfig readConfigFromFile(String configFileName) {
+        InputStream fileStream = openFileAsInputStream(configFileName);
+        Yaml yaml = new Yaml();
+        MarshalConfig config=null;
 
+        try {
+            logger.error("start load config:{}",configFileName);
+            config = yaml.loadAs(fileStream, MarshalConfig.class);
+
+        } catch (Exception e) {
+
+            // 记录日志...
+        }
+
+        return config;
+    }
+
+
+    @Getter
+    @Setter
+    public static class MarshalBasicConfig {
+        private Boolean standalone;
+        private Integer serverPort;
+
+    }
 
 
 }
--- marshal-server/src/main/java/com/chuhui/marshal/MarshalBootstrap.java
+++ marshal-server/src/main/java/com/chuhui/marshal/MarshalBootstrap.java
@@ -1,5 +1,12 @@
 package com.chuhui.marshal;
 
+import com.chuhui.marshal.framework.utils.config.MarshalConfig;
+import com.chuhui.marshal.framework.utils.config.MarshalConfig.MarshalBasicConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URL;
+
 /**
  * MarshalBootstrap
  * <p>
@@ -9,36 +16,48 @@ package com.chuhui.marshal;
  * @Date: 2019/11/13 0013
  * @Description:TODO
  */
+
 public class MarshalBootstrap {
 
+    final static  private Logger logger = LoggerFactory.getLogger(MarshalBootstrap.class);
+
     private static final String USAGE =
-            "Usage: MarshalBootstrap configfile ";
-
-
+            "Usage: MarshalBootstrap configFile ";
 
     public static void main(String[] args) {
 
-        if(args==null || args.length<=0){
+
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        URL resource = contextClassLoader.getResource("log4j.properties");
+
+        if (args == null || args.length <= 0) {
             System.err.println("Please specify the profile location");
             System.err.println(USAGE);
-            return ;
+            return;
         }
 
         MarshalBootstrap marshal = new MarshalBootstrap();
-
-
         marshal.initConfig(args);
 
-        // 集群和单机模式
-        //
+
+
     }
 
     private void initConfig(String[] args) {
 
 
+        URL resource = getClass().getClassLoader().getResource("log4j.properties");
 
 
+        logger.info("start resolve config");
+        MarshalConfig init = MarshalConfig.readConfigFromFile(args[0]);
 
+        MarshalBasicConfig marshalBasic = init.getMarshalBasic();
+
+        if(marshalBasic==null){
+            // 记录日志...
+            logger.error("can't  resolved config to "+MarshalBasicConfig.class.getName());
+        }
 
     }
 
